87a7b4c0daa058443ab581739acea2c9d4aa0882
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-12-25 20:55:42 +0100

    Fixed typo

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className={css.hero}>
             <div className={css.hero__wrapper}>
-                <h1 className={css.hero__title}>Krzysztof Adamczakąęśźć</h1>
+                <h1 className={css.hero__title}>Krzysztof Adamczak</h1>
                 <h2 className={css.hero__subtitle}>Front End Developer</h2>
                 <Button to='projects'>Zobacz moje portfolio</Button>
             </div>
