9affb0713ed20115ef113c660af98caad16f4a84
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-12-26 22:52:24 +0100

    Small visual changes

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
 import css from "../../styles/Contact.module.css";
+import buttonCss from "../../styles/Button.module.css";
 import Button from "./Button";
 
 const Contact = () => {
     const aRef = useRef<HTMLAnchorElement>(null);
     const handleEmail = () => {
-        if (aRef === null && aRef === undefined) return;
+        if (aRef == null) return;
 
         aRef.current?.setAttribute("href", "mailto: krzadamczak@gmail.com");
         console.log(aRef.current?.href);
@@ -13,7 +14,7 @@ const Contact = () => {
     return (
         <section className={css.contact}>
             <p className={css.contact__text}>Zainteresowany podjęciem współpracy?</p>{" "}
-            <a ref={aRef} onClick={handleEmail}>
+            <a className={buttonCss.button} ref={aRef} onClick={handleEmail}>
                 Napisz do mnie!
             </a>
         </section>
